c22fc5fbf997f8b8869e61ee878555475c707504
SaNaTwT <115217603+SaNaTwT@users.noreply.github.com>
2022-12-14 11:53:57 +0100

    js + json

---
+++ js/code.js
@@ -0,0 +1,11 @@
+class Anime
+{
+    constructor(title, genre, author, epnumber,hebergeur)
+    {
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.epnumber = epnumber;
+        this.hebergeur = hebergeur;
+    }
+}
