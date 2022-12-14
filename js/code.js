f51d9084897a23f69ed7d66370c0b92d7950e69e
SaNaTwT <115217603+SaNaTwT@users.noreply.github.com>
2022-12-14 12:52:50 +0100

    ajout tableau + form + js pas finis

--- js/code.js
+++ js/code.js
@@ -9,3 +9,22 @@ class Anime
         this.hebergeur = hebergeur;
     }
 }
+
+function ajouterAnime()
+{
+    var newAnime = animeSaisie();
+    pushTache(newTask);
+    var newLine = document.createElement("tr");
+    var nameTask = document.createElement("td");
+    var dateTask = document.createElement("td");
+    var catTask = document.createElement("td");
+    nameTask.textContent = newTask.nom;
+    dateTask.textContent = newTask.date;
+    catTask.textContent = newTask.categorie;
+    newLine.append(nameTask, dateTask, catTask);
+    var table = document.querySelector(".datatable tbody");
+    if (nameTask.textContent != "" && dateTask.textContent != "" && catTask.textContent != "")
+    {
+        table.appendChild(newLine);
+    }
+}
