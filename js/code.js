8ed9f15f2be394cd47eebe032f19fbe48f835dab
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2022-12-23 16:00:13 +0100

    update

--- js/code.js
+++ js/code.js
@@ -13,7 +13,7 @@ class Anime
 function ajouterAnime()
 {
     var newAnime = animeSaisie();
-    pushTache(newTask);
+    pushTache(newAnime);
     var newLine = document.createElement("tr");
     var nameTask = document.createElement("td");
     var dateTask = document.createElement("td");
@@ -22,9 +22,27 @@ function ajouterAnime()
     dateTask.textContent = newTask.date;
     catTask.textContent = newTask.categorie;
     newLine.append(nameTask, dateTask, catTask);
-    var table = document.querySelector(".datatable tbody");
+    var table = document.querySelector("liste");
     if (nameTask.textContent != "" && dateTask.textContent != "" && catTask.textContent != "")
     {
         table.appendChild(newLine);
+    }
+}
+
+
+function getAnimeByAPI()
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.jikan.moe/v3/search/anime?q=one%20piece");
+    xhr.send();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState == 4 && xhr.status == 200)
+        {
+            var anime = JSON.parse(xhr.responseText);
+            console.log(anime);
+            var anime = new Anime(anime.title, anime.genre, anime.author, anime.epnumber, anime.hebergeur);
+            console.log(anime);
+        }
     }
 }
