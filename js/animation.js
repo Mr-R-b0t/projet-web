ef103c04a49c9cced13ba9ae12c0d058b4cef552
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2022-12-27 22:44:44 +0100

    update

--- js/animation.js
+++ js/animation.js
@@ -23,3 +23,33 @@ window.onscroll = function () {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
 }
+
+
+const menuBtn = document.querySelector(".menu-btn");
+const menuContainer = document.getElementById("menu");
+const timeLine = document.getElementById("timeline");
+const footer = document.getElementById("footer");
+const bodyAbout = document.getElementById("body-about");
+
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+    menuContainer.classList.add("visible");
+    timeLine?.classList.remove("visible-section");
+    footer?.classList.remove("visible-section");
+    timeLine?.classList.add("hidden-timeline");
+    footer?.classList.add("hidden-footer");
+    bodyAbout?.classList.add("hidden-about");
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+    menuContainer?.classList.remove("visible");
+    timeLine?.classList.add("visible-section");
+    footer?.classList.add("visible-section");
+    timeLine?.classList.remove("hidden-timeline");
+    footer?.classList.remove("hidden-footer");
+    bodyAbout?.classList.remove("hidden-about");
+  }
+});
